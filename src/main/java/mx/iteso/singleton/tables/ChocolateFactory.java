ec7f1123cdbeb01c869bb3dd1ede82a7b2ff864f
Rextype15 <rextype15@gmail.com>
2015-11-30 00:41:03 -0600

    mend

---
+++ src/main/java/mx/iteso/singleton/tables/ChocolateFactory.java
@@ -0,0 +1,14 @@
+package mx.iteso.singleton.tables;
+
+import mx.iteso.singleton.ChocolateBoiler;
+
+/**
+ * Created by Emmanuel-GPC on 11/30/2015.
+ */
+public class ChocolateFactory {
+
+    public void main (String args[]){
+        ChocolateBoiler cb1 = new ChocolateBoiler();
+
+    }
+}
